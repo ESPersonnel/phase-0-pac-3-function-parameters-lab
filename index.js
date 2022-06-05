59ecbf6e02e616542c515d4c2a3f90a067a699a5
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-05 13:44:08 +0300

    commit tree c4c6e2dae84c14bc32721d64dd20e002f561ce1d

---
+++ index.js
@@ -0,0 +1,16 @@
+// Define a function called introduction that defines a parameter, name, and returns the phrase: "Hi, my name is ${name}."
+
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+// define a function called introductionWithLanguage that defines two parameters, name and language, and returns the phrase: "Hi, my name is [name] and I am learning to program in [language]."
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+// Copy the function you created for the second test and name it introductionWithLanguageOptional. It should have two parameters, name and language, and the second parameter should have a default value of "JavaScript".
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
