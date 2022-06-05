c732222c07bc30f2494a6688b2a726ee02da720a
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-05 13:45:02 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+// Code 1
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+// Code 2
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+// Code 3 and 4
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
